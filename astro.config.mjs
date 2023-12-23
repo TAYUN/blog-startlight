2c40e6290053191b03f0c2d8e6d56c4b88ab30f3
xiaoyun <507028106@qq.com>
2023-12-23 18:20:34 +0800

    测试

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tayunblog.github.io',
-	base: '/tayunblog.github.io', 
+	base: '/', 
 	integrations: [
 		starlight({
 			title: '首页',
